2514230c2435aab1e2c7e9aadb471d27b8ea2c6f
RadoRM <radomiarintsoarakotozafy@gmail.com>
2019-04-12 17:20:36 +0200

    Initial commit

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+
+  namespace AppBundle\Entity;
+
+  use Doctrine\ORM\Mapping as ORM;
+  use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="`user`")
+ */
+  class User extends BaseUser
+  {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+  }
